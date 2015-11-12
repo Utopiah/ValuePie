9940ece420536eb3ca647d3551cc16abd4099759
Fabien Benetou <fabien-services@benetou.fr>
2015-11-12 11:40:19 +0100

    First draft of Django models

---
+++ Models.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+# represents a good or a service
+class Valuable(models.Model):
+	name = models.CharField(max_length=100)
+	description = models.CharField(max_length=500)
+	shop = models.ForeignKey(Shop)
+
+class Shop(models.Model):
+	name = models.CharField(max_length=100)
+	description = models.CharField(max_length=500)
+	url = models.URLField()
+	email = models.EmailField()
+	payment_system = models.CharField(max_length=100)
+	# e.g. IBAN or Bitcoin address
+
+class ValuablePriceSegment(models.Model):
+	name = models.CharField(max_length=50)
+	description = models.CharField(max_length=500)
+	minimum_price = models.IntField()
+	minimum_percentage = models.IntField()
+	valuable = models.ForeignKey(Valuable)
+#	W = models.ManyToManyField(X) consider instead
+	payment_system = models.CharField(max_length=100)
+	# e.g. IBAN or Bitcoin address
+
+class ValuedPurchase(models.Model):
+	valuable_price_segment = models.ForeignKey(ValuablePriceSegment)
+	client = models.ForeignKey(Client)
+	amount = models.IntField()
+	percentage = models.IntField()
+	sale = models.ForeignKey(Sales)
+
+class Sales(models.Model):
+	shop = models.ForeignKey(Shop)
+	client = models.ForeignKey(Client)
+	date = models.DateTime()
+
+class Stock(models.Model):
+	valuable = models.ForeignKey(Valuable)
+	shop = models.ForeignKey(Shop)
+	quantity = models.IntField()
+
+class Client(models.Model):
+	name = models.CharField(max_length=50)
+	email = models.EmailField()
+	payment_system = models.CharField(max_length=100)
